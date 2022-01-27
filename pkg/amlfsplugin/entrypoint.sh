5b31d5da3f4c20c527064efea05beae67933ef21
Justin Jin <jusjin@microsoft.com>
2022-01-27 11:15:40 +0800

    refactor entrypoint.sh with lctl workaround

--- pkg/amlfsplugin/entrypoint.sh
+++ pkg/amlfsplugin/entrypoint.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
-#
-# $1 is the path to the CSI driver.
-#
-echo $@
-apt-get update
 
-if [[ "$(uname -r)" = "5.4.0-1059-azure" ]]; then
-    echo "Installing for 1059"
-    apt-get install -y libreadline7 kmod fdutils libkeyutils-dev "$PKG1" "$PKG2"
-elif [[ "$(uname -r)" = "5.4.0-1063-azure" ]]; then
-    echo "Installing for 1063"
-    apt-get install -y libreadline7 kmod fdutils libkeyutils-dev "$PKG1_1063" "$PKG2_1063"
-else
-    echo "Installing for 1064"
-    apt-get install -y libreadline7 kmod fdutils libkeyutils-dev "$PKG1_1064" "$PKG2_1064"
-fi
+#
+# Shell script to install Lustre client kernel modules and launch CSI driver
+#   $1 is the path to the CSI driver.
+#
+
+set -o xtrace
+set -o errexit
+set -o pipefail
+set -o nounset
+
+echo Command line arguments: $@
+
+apt-get update -y
+apt-get install -y libreadline7 libkeyutils1 "$PKG1_1064" "$PKG2_1064"
 
 modprobe -v ksocklnd
 modprobe -v lnet
 modprobe -v mgc
 modprobe -v lustre
-lctl network up
-#mkdir -p /host_tmp/lustre
-#mount -t lustre $MDS_IP_ADDR@tcp:/lustrefs /host_tmp/lustre
+
+# For some reason, this is a false positive before we restart the container
+# The volume mount succeeds later even this returns a failure
+# We need to revisit this after moving the script to run on AKS node
+lctl network up || true
+
 echo "<Lustre CSI driver>"
-echo Executing: $1 $2 $3 $4 $5 $6 $7 $8 $9
-$1 $2 $3 $4 $5 $6 $7 $8 $9
+echo Executing: $1 ${2-} ${3-} ${4-} ${5-} ${6-} ${7-} ${8-} ${9-}
+$1 ${2-} ${3-} ${4-} ${5-} ${6-} ${7-} ${8-} ${9-}
 echo "</Lustre CSI driver>"
