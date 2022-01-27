c06785fadfa3ef8b9f1ec4068c4af05a9e364f12
Justin Jin <jusjin@microsoft.com>
2022-01-27 18:01:58 +0800

    apt-get in dockerfile

--- pkg/amlfsplugin/entrypoint.sh
+++ pkg/amlfsplugin/entrypoint.sh
@@ -15,12 +15,10 @@ echo Command line arguments: $@
 urlPrefix="https://amlfscsiinfrasa.blob.core.windows.net/lustre-client-module/canonical/ubuntuserver/18.04-lts"
 kernelVersion=$(uname -r)
 
-apt-get update -y
-apt-get install -y libreadline7 libkeyutils1 wget
-
 wget "${urlPrefix}/${kernelVersion}/lustre-client-utils_2.14.0_amd64.deb"
 wget "${urlPrefix}/${kernelVersion}/lustre-client-modules_2.14.0_amd64.deb"
 
+apt-get update -y
 apt-get install -y "./lustre-client-utils_2.14.0_amd64.deb" "./lustre-client-modules_2.14.0_amd64.deb"
 
 apt-get autoremove -y wget
