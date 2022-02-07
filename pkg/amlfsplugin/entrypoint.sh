826fa12f5c251c9f6e7baec78f53b23e800637a5
Justin Jin <jusjin@microsoft.com>
2022-02-07 13:00:26 +0800

    feat: refactor Dockerfile/entrypoint.sh and implement dynamic client module installation from blob container (#10)
    * temp for dynamic client install
    
    * working 1063
    
    * build.sh update
    
    * working remote run-command to build lustre anyways
    
    * good 1064 tested (manual built)
    
    * work?
    
    * working libkeyutils-dev and kubenet vnet peering
    
    * refactor entrypoint.sh with lctl workaround
    
    * dynamic install client module from blob container
    
    * apt-get in dockerfile
    
    * remove -f for apt update
    
    * working version for 1059, 1063, 1064 and 1065
    
    * revert test change

--- pkg/amlfsplugin/entrypoint.sh
+++ pkg/amlfsplugin/entrypoint.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
+
 #
-# $1 is the path to the CSI driver.
+# Shell script to install Lustre client kernel modules and launch CSI driver
+#   $1 is the path to the CSI driver.
 #
-echo $@
+
+set -o xtrace
+set -o errexit
+set -o pipefail
+set -o nounset
+
+echo Command line arguments: $@
+
+urlPrefix="https://amlfscsiinfrasa.blob.core.windows.net/lustre-client-module/canonical/ubuntuserver/18.04-lts"
+kernelVersion=$(uname -r)
+
+wget "${urlPrefix}/${kernelVersion}/lustre-client-utils_2.14.0_amd64.deb"
+wget "${urlPrefix}/${kernelVersion}/lustre-client-modules_2.14.0_amd64.deb"
+
 apt-get update
-apt-get install -y libreadline7 kmod fdutils "$PKG1" "$PKG2"
+apt-get install -y "./lustre-client-utils_2.14.0_amd64.deb" "./lustre-client-modules_2.14.0_amd64.deb"
+
+apt-get autoremove -y wget
+
+rm --force ./lustre-client-utils_2.14.0_amd64.deb
+rm --force ./lustre-client-modules_2.14.0_amd64.deb
 
 modprobe -v ksocklnd
 modprobe -v lnet
 modprobe -v mgc
 modprobe -v lustre
-lctl network up
-#mkdir -p /host_tmp/lustre
-#mount -t lustre $MDS_IP_ADDR@tcp:/lustrefs /host_tmp/lustre
+
+# For some reason, this is a false positive before we restart the container
+# The volume mount succeeds later even this returns a failure
+# We need to revisit this after moving the script to run on AKS node
+lctl network up || true
+
 echo "<Lustre CSI driver>"
-echo Executing: $1 $2 $3 $4 $5 $6 $7 $8 $9
-$1 $2 $3 $4 $5 $6 $7 $8 $9
-echo "</Lustre CSI driver>"/
+echo Executing: $1 ${2-} ${3-} ${4-} ${5-} ${6-} ${7-} ${8-} ${9-}
+$1 ${2-} ${3-} ${4-} ${5-} ${6-} ${7-} ${8-} ${9-}
+echo "</Lustre CSI driver>"
