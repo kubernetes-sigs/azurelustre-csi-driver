aa9384720eb53920fa8d94786a3e5026ddc6e3fc
Chuanjun Yin <woshifuxiuyin@gmail.com>
2022-04-10 20:42:04 +0800

    download kubectl

--- test/external-e2e/run.sh
+++ test/external-e2e/run.sh
@@ -29,6 +29,10 @@ update-ca-certificates
 
 PROJECT_ROOT=$(pwd)
 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o kubectl
+chmod +x kubectl
+cp ./kubectl /usr/bin/
+
 curl -sL https://storage.googleapis.com/kubernetes-release/release/v1.22.0/kubernetes-test-linux-amd64.tar.gz --output e2e-tests.tar.gz
 tar -xvf e2e-tests.tar.gz && rm e2e-tests.tar.gz
 
@@ -44,7 +48,7 @@ mkdir -p /tmp/csi
 echo "begin to run amlfs tests ...."
 cp $PROJECT_ROOT/test/external-e2e/e2etest_storageclass.yaml /tmp/csi/storageclass.yaml
 ginkgo -p --progress --v -focus="External.Storage.*.amlfs.csi.azure.com" \
+    -skip="should access to two volumes with the same volume mode and retain data across pod recreation on the same node|should support two pods which share the same volume|should be able to unmount after the subpath directory is deleted|should support two pods which share the same volume|Should test that pv written before kubelet restart is readable after restart|should unmount if pod is force deleted while kubelet is down|should unmount if pod is gracefully deleted while kubelet is down" \
     kubernetes/test/bin/e2e.test  -- \
-    -skip="should access to two volumes with the same volume mode and retain data across pod recreation on the same node|should support two pods which share the same volume|should be able to unmount after the subpath directory is deleted|should support two pods which share the same volume|Should test that pv written before kubelet restart is readable after restart|should unmount if pod is force deleted while kubelet is down|should unmount if pod is gracefully deleted while kubelet is down"
     -storage.testdriver=$PROJECT_ROOT/test/external-e2e/testdriver-amlfs.yaml \
     --kubeconfig=$KUBECONFIG
