137e6fffda8f3ae24a83f55e29583fdf0d89838a
Chuanjun Yin <woshifuxiuyin@gmail.com>
2022-05-25 21:53:27 +0800

    [chore] disable the unused metrics server (#37)
    * disable unused metrics server
    
    * update go mod file

--- pkg/azurelustreplugin/main.go
+++ pkg/azurelustreplugin/main.go
@@ -17,17 +17,12 @@ limitations under the License.
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
-	"net"
-	"net/http"
 	"os"
-	"strings"
 
 	"sigs.k8s.io/azurelustre-csi-driver/pkg/azurelustre"
 
-	"k8s.io/component-base/metrics/legacyregistry"
 	"k8s.io/klog/v2"
 )
 
@@ -39,7 +34,6 @@ var (
 	endpoint                   = flag.String("endpoint", "unix://tmp/csi.sock", "CSI endpoint")
 	nodeID                     = flag.String("nodeid", "", "node id")
 	version                    = flag.Bool("version", false, "Print the version and exit.")
-	metricsAddress             = flag.String("metrics-address", "0.0.0.0:29634", "export the metrics")
 	driverName                 = flag.String("drivername", azurelustre.DefaultDriverName, "name of the driver")
 	enableAzureLustreMockMount = flag.Bool("enable-azurelustre-mock-mount", false, "Whether enable mock mount(only for testing)")
 )
@@ -56,7 +50,6 @@ func main() {
 		os.Exit(0)
 	}
 
-	exportMetrics()
 	handle()
 	os.Exit(0)
 }
@@ -72,42 +65,4 @@ func handle() {
 		klog.Fatalln("Failed to initialize Azure Lustre CSI driver")
 	}
 	driver.Run(*endpoint, "", false)
-}
-
-func exportMetrics() {
-	l, err := net.Listen("tcp", *metricsAddress)
-	if err != nil {
-		klog.Warningf("failed to get listener for metrics endpoint: %v", err)
-		return
-	}
-	serve(context.Background(), l, serveMetrics)
-}
-
-func serve(ctx context.Context, l net.Listener, serveFunc func(net.Listener) error) {
-	path := l.Addr().String()
-	klog.V(2).Infof("set up prometheus server on %v", path)
-	go func() {
-		defer l.Close()
-		if err := serveFunc(l); err != nil {
-			klog.Fatalf("serve failure(%v), address(%v)", err, path)
-		}
-	}()
-}
-
-func serveMetrics(l net.Listener) error {
-	m := http.NewServeMux()
-
-	// azure cloud provider uses legacyregistry currently
-	m.Handle("/metrics", legacyregistry.Handler()) //nolint
-	return trapClosedConnErr(http.Serve(l, m))
-}
-
-func trapClosedConnErr(err error) error {
-	if err == nil {
-		return nil
-	}
-	if strings.Contains(err.Error(), "use of closed network connection") {
-		return nil
-	}
-	return err
 }
