a5ab5725e64c4bba69469004db5bfa33f00ec1f3
Chuanjun Yin <woshifuxiuyin@gmail.com>
2022-10-17 15:25:43 +0800

    add external e2e to long haul

--- test/long-haul/external_e2e.sh
+++ test/long-haul/external_e2e.sh
@@ -14,8 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-python $(pwd)/../scale/run_test.py --provisioning-type static \
-                                   --scales 4 8 16 32 64 128 256 \
-                                   --csi-name azurelustre.csi.azure.com \
-                                   --mgs-ip-address ${LustreFSIP:-"172.18.32.5"} \
-                                   --fs-name lustrefs
+${REPO_ROOT_PATH}/test/external-e2e/run.sh
