5da8c361f826ba01895646d59ed8a848081156f5
Chuanjun Yin <woshifuxiuyin@gmail.com>
2022-11-13 21:52:14 +0800

    update e2e test version

--- test/external-e2e/run.sh
+++ test/external-e2e/run.sh
@@ -22,11 +22,12 @@ set -o xtrace
 REPO_ROOT_PATH=${REPO_ROOT_PATH:-$(git rev-parse --show-toplevel)}
 KUBECONFIG=${KUBECONFIG:-$(echo "$HOME/.kube/config")}
 echo "GO path $(which go)"
+echo "ginkgo path $(which ginkgo) and version $(ginkgo version)"
 echo "kubectl path $(which kubectl)"
 echo "REPO_ROOT_PATH ${REPO_ROOT_PATH}"
 echo "KUBECONFIG path ${KUBECONFIG}"
 
-curl -sL https://storage.googleapis.com/kubernetes-release/release/v1.22.0/kubernetes-test-linux-amd64.tar.gz --output e2e-tests.tar.gz
+curl -sL https://storage.googleapis.com/kubernetes-release/release/v1.24.6/kubernetes-test-linux-amd64.tar.gz --output e2e-tests.tar.gz
 tar -xvf e2e-tests.tar.gz && rm e2e-tests.tar.gz
 
 sc_file="${REPO_ROOT_PATH}/test/external-e2e/e2etest_storageclass.yaml"
@@ -45,6 +46,7 @@ trap clean_up_and_print_logs EXIT
 mkdir -p /tmp/csi
 
 # reclaim policy test
+
 echo "begin to test reclaim policy"
 echo "deploy test storageclass with default reclaim policy (delete)"
 kubectl apply -f ${sc_file}
@@ -62,13 +64,13 @@ kubectl wait --for=delete -f ${claim_file} --timeout=300s
 echo "wait for pv ${bounded_pv} to be deleted"
 kubectl wait --for=delete pv/${bounded_pv} --timeout=300s
 
-echo "delete test storageclass"
-kubectl delete -f ${sc_file}
+# echo "delete test storageclass"
+# kubectl delete -f ${sc_file}
 
 echo "begin to run azurelustre tests ...."
 cp ${REPO_ROOT_PATH}/test/external-e2e/e2etest_storageclass.yaml /tmp/csi/storageclass.yaml
 ginkgo -p --progress --v -focus="External.Storage.*.azurelustre.csi.azure.com" \
-    -skip="should access to two volumes with the same volume mode and retain data across pod recreation on the same node|should support two pods which share the same volume|should be able to unmount after the subpath directory is deleted|should support two pods which share the same volume|Should test that pv written before kubelet restart is readable after restart|should unmount if pod is force deleted while kubelet is down|should unmount if pod is gracefully deleted while kubelet is down" \
+    -skip="should access to two volumes with the same volume mode and retain data across pod recreation on the same node|should support two pods which share the same volume|should be able to unmount after the subpath directory is deleted|should support two pods which share the same volume|Should test that pv written before kubelet restart is readable after restart|should unmount if pod is force deleted while kubelet is down|should unmount if pod is gracefully deleted while kubelet is down|should support two pods which have the same volume definition|should access to two volumes with the same volume mode and retain data across pod recreation on different node" \
     kubernetes/test/bin/e2e.test  -- \
     -storage.testdriver=${REPO_ROOT_PATH}/test/external-e2e/testdriver-azurelustre.yaml \
     --kubeconfig=${KUBECONFIG}
