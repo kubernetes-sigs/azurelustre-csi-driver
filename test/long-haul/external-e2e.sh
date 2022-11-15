f55380f53ddc0bdca2cd69fa1b2de7c1a42f5143
Chuanjun Yin <woshifuxiuyin@gmail.com>
2022-11-15 15:32:06 +0800

    dynamic test version and mgs ip address

--- test/long-haul/external-e2e.sh
+++ test/long-haul/external-e2e.sh
@@ -17,17 +17,27 @@
 set -o errexit
 set -o pipefail
 set -o nounset
+set -o xtrace
+
+export kubernetesVersion=v$(az aks list | jq -r ".[0].currentKubernetesVersion")
+echo "Current kubernetes version is ${kubernetesVersion}"
+
+echo "Downloading kubectl ${kubernetesVersion}"
+curl -Lo ${REPO_ROOT_PATH}/kubectl "https://dl.k8s.io/release/${kubernetesVersion}/bin/linux/amd64/kubectl"
+chmod a+x ${REPO_ROOT_PATH}/kubectl
+export PATH=$(pwd):${PATH}
+
+goVersion=$(kubectl version --client=false -ojson | jq -r ".serverVersion.goVersion")
+echo "Kubernetes go version is ${goVersion}"
 
 echo "Installing go and ginkgo"
-curl -Lo ${REPO_ROOT_PATH}/go1.19.2.linux-amd64.tar.gz https://go.dev/dl/go1.19.2.linux-amd64.tar.gz
-tar -zxf ${REPO_ROOT_PATH}/go1.19.2.linux-amd64.tar.gz -C ${REPO_ROOT_PATH}/
+curl -Lo ${REPO_ROOT_PATH}/${goVersion}.linux-amd64.tar.gz https://go.dev/dl/${goVersion}.linux-amd64.tar.gz
+tar -zxf ${REPO_ROOT_PATH}/${goVersion}.linux-amd64.tar.gz -C ${REPO_ROOT_PATH}/
 export GOBIN=${REPO_ROOT_PATH}/go/bin
 export PATH=${GOBIN}:${PATH}
 go install github.com/onsi/ginkgo/ginkgo@v1.16.5
 
-echo "Downloading latest kubectl"
-curl -Lo ${REPO_ROOT_PATH}/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod a+x ${REPO_ROOT_PATH}/kubectl
-export PATH=$(pwd):${PATH}
+export LUSTRE_FS_NAME=${LustreFSName}
+export LUSTRE_MGS_IP=${LustreFSIP}
 
 ${REPO_ROOT_PATH}/test/external-e2e/run.sh
