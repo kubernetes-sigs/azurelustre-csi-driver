e2bc5246f0d6b68595ac41aaf3ded0525625e4f1
Chuanjun Yin <woshifuxiuyin@gmail.com>
2023-02-28 16:31:07 +0800

    add dely and retry as workaround (#102)

--- pkg/azurelustreplugin/fix-lnet.sh
+++ pkg/azurelustreplugin/fix-lnet.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-(
-  flock -w 60 -e ${FD}
-  if sudo lnetctl net show --net tcp | grep "status: down"; then
-    /usr/sbin/lnetctl net del --net tcp
-    /usr/sbin/lnetctl net add --net tcp --if {default_interface}
-  fi
-) {FD}< /etc/lustre/.lock
+for run_id in 0 2 4; do
+  sleep $run_id
+  (
+    flock -w 60 -e ${FD}    
+    if sudo lnetctl net show --net tcp | grep "status: down"; then
+      /usr/sbin/lnetctl net del --net tcp
+      /usr/sbin/lnetctl net add --net tcp --if {default_interface}
+    fi
+  ) {FD}< /etc/lustre/.lock
+done
