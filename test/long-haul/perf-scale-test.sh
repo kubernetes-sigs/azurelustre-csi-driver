dae228a60cd9f6c9953951ffc62a9520f1504260
Vincent Li <30455256+vinli-cn@users.noreply.github.com>
2023-03-09 18:46:17 +0800

    Fix per-scale-test (#113)

--- test/long-haul/perf-scale-test.sh
+++ test/long-haul/perf-scale-test.sh
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# disable scales 32 64 128 256 for Lnet fix \
 python $(pwd)/../scale/run_test.py --provisioning-type static \
-                                   # disable scales 32 64 128 256 for Lnet fix \
                                    --scales 4 8 16 \ 
                                    --csi-name azurelustre.csi.azure.com \
                                    --mgs-ip-address ${LustreFSIP:-"172.18.32.5"} \
