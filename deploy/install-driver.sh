79271964e93483704863e1b30495f13a369b8b6a
Chris Mueller <chris@csmueller.com>
2024-03-13 12:47:10 -0400

    Harden install-driver.sh and add remote options.

--- deploy/install-driver.sh
+++ deploy/install-driver.sh
@@ -13,33 +13,60 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -o xtrace
 set -euo pipefail
 
-ver="main"
-if [[ "$#" -gt 0 ]]; then
-  ver="$1"
+function usage {
+    echo "Usage: $0 [branch|local|url]"
+    echo
+    echo "branch: The branch from which to install the Azure Lustre CSI Driver to install. Default is 'main'."
+    echo "local: Deploy out of local filesystem."
+    echo
+    echo "Example:"
+    echo "$0 # install from remote main"
+    echo "$0 main # install from remote branch or reference"
+    echo "$0 local # install from locally checked out branch"
+    echo "$0 https://raw.githubusercontent.com/csmuell/azurelustre-csi-driver/main # install from given remote repository/branch"
+    exit 1
+}
+
+if [[ "$#" -gt 1 || ("$#" -gt 0 && "$1" == "--help") ]]; then
+  usage
 fi
 
-repo="https://raw.githubusercontent.com/kubernetes-sigs/azurelustre-csi-driver/$ver/deploy"
+branch="main"
+repo="https://raw.githubusercontent.com/kubernetes-sigs/azurelustre-csi-driver/${branch}/deploy"
 
-if [[ "$#" -gt 1 ]]; then
-  if [[ "$2" == *"local"* ]]; then
-    echo "use local deploy"
-    repo="$(git rev-parse --show-toplevel)/deploy"
+if [[ "$#" -eq 1 ]]; then
+  case "$1" in
+    local)
+      repo="$(git rev-parse --show-toplevel)/deploy"
+      ;;
+    http*)
+      repo="${1}/deploy"
+      ;;
+    *)
+      branch="${1}"
+      repo="https://raw.githubusercontent.com/kubernetes-sigs/azurelustre-csi-driver/${branch}/deploy"
+      ;;
+  esac
+fi
+
+verify="${repo}/install-driver.sh"
+if ! [ -f "${verify}" ]; then
+  if ! curl -L -Is --fail "${verify}" > /dev/null; then
+    echo "Unknown repository: ${repo} ${verify} does not exist."
+    usage
   fi
 fi
 
-# if [ $ver != "main" ]; then
-#   repo="$repo/$ver"
-# fi
+echo
+echo "Installing Azure Lustre CSI Driver branch: $branch, repo: $repo ..."
 
-echo "Installing Azure Lustre CSI driver, version: $ver, repo: $repo ..."
-kubectl apply -f $repo/rbac-csi-azurelustre-controller.yaml
-kubectl apply -f $repo/rbac-csi-azurelustre-node.yaml
-kubectl apply -f $repo/csi-azurelustre-driver.yaml
-kubectl apply -f $repo/csi-azurelustre-controller.yaml
-kubectl apply -f $repo/csi-azurelustre-node.yaml
+kubectl apply -f "$repo/rbac-csi-azurelustre-controller.yaml"
+kubectl apply -f "$repo/rbac-csi-azurelustre-node.yaml"
+kubectl apply -f "$repo/csi-azurelustre-driver.yaml"
+kubectl apply -f "$repo/csi-azurelustre-controller.yaml"
+kubectl apply -f "$repo/csi-azurelustre-node.yaml"
 
 kubectl rollout status deployment csi-azurelustre-controller -nkube-system --timeout=300s
 kubectl rollout status daemonset csi-azurelustre-node -nkube-system --timeout=1800s
