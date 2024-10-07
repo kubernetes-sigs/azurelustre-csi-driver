99f945cdbf8f0eea86202774d134bab4cb040461
David A. Bradley <dabradley@users.noreply.github.com>
2024-10-07 15:28:44 -0400

    Fix style issues

--- pkg/csi-common/utils.go
+++ pkg/csi-common/utils.go
@@ -34,7 +34,7 @@ func ParseEndpoint(ep string) (string, string, error) {
 			return s[0], s[1], nil
 		}
 	}
-	return "", "", fmt.Errorf("Invalid endpoint: %v", ep)
+	return "", "", fmt.Errorf("invalid endpoint: %v", ep)
 }
 
 func NewVolumeCapabilityAccessMode(mode csi.VolumeCapability_AccessMode_Mode) *csi.VolumeCapability_AccessMode {
--- pkg/util/util.go
+++ pkg/util/util.go
@@ -144,11 +144,11 @@ func ConvertTagsToMap(tags string) (map[string]string, error) {
 	for _, tag := range s {
 		kv := strings.Split(tag, tagKeyValueDelimiter)
 		if len(kv) != 2 {
-			return nil, fmt.Errorf("Tags '%s' are invalid, the format should like: 'key1=value1,key2=value2'", tags)
+			return nil, fmt.Errorf("tags '%s' are invalid, the format should like: 'key1=value1,key2=value2'", tags)
 		}
 		key := strings.TrimSpace(kv[0])
 		if key == "" {
-			return nil, fmt.Errorf("Tags '%s' are invalid, the format should like: 'key1=value1,key2=value2'", tags)
+			return nil, fmt.Errorf("tags '%s' are invalid, the format should like: 'key1=value1,key2=value2'", tags)
 		}
 		value := strings.TrimSpace(kv[1])
 		m[key] = value
