35416550193c4ef65e99f80882b54d807ed300ed
David A. Bradley <dabradley@users.noreply.github.com>
2025-08-01 10:08:41 -0400

    Don't skip tests

--- test/external-e2e/run.sh
+++ test/external-e2e/run.sh
@@ -78,7 +78,6 @@ kubectl delete -f ${sc_file}
 echo "begin to run azurelustre tests ...."
 cp ${REPO_ROOT_PATH}/test/external-e2e/e2etest_storageclass.yaml /tmp/csi/storageclass.yaml
 ${REPO_ROOT_PATH}/kubernetes/test/bin/ginkgo -p -v -focus="External.Storage.*.azurelustre.csi.azure.com" \
-    -skip="should access to two volumes with the same volume mode and retain data across pod recreation on the same node|should support two pods which have the same volume definition|should access to two volumes with the same volume mode and retain data across pod recreation on different node" \
     ${REPO_ROOT_PATH}/kubernetes/test/bin/e2e.test  -- \
     -storage.testdriver=${REPO_ROOT_PATH}/test/external-e2e/testdriver-azurelustre.yaml \
     --kubeconfig=${KUBECONFIG}
